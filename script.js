095f67fee0df3342babfa51be018081637c29285
Fouzia ifat <fouzia786ifat@gmail.com>
2025-01-11 15:19:15 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let myBtn = document.getElementById("myBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    myBtn.classList.add("show");
+  } else {
+    myBtn.classList.remove("show");
+  }
+};
+
 const headerBoxes = document.querySelectorAll(".demo-image .img-scroll");
 headerBoxes.forEach((box) => {
   box.addEventListener("mouseenter", () => {
